bf7429a1477f487d124aaea0035cfa09afc2c18d
Nick Semenkovich <semenko@alum.mit.edu>
2011-09-21 16:28:37 -0500

    Updated git status tester that prints logs.

--- git-status-tester.sh
+++ git-status-tester.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 # Author: Nick Semenkovich <semenko@alum.mit.edu>
 
-gitstatus=`git status -s | grep -v "^ M " | grep -v "^ ?? " `
+git remote update 2>&1 >/dev/null
+
+gitstatus=`git log master..origin/master`
 
 if [ "$gitstatus" ]
 then
     if tty -s
     then
 	USER_TTY=`tty`
-	echo "\a@@@ WARNING @@@:\nYou are using an outdated version of this software." > $USER_TTY
-	echo "There is an updated version available in git.\n" > $USER_TTY
-	echo "Please run:\n\t git pull" > $USER_TTY
+	echo "\n\a\t\t@@@ WARNING @@@ - This software is outdated." > $USER_TTY
+	echo "\n\nThere is an updated version in git with these changes:\n" > $USER_TTY
+	echo "$gitstatus" > $USER_TTY
+	echo "\n\n\t\t@@@ Please run: git pull @@@\n" > $USER_TTY
 	sleep 2
     fi
 fi
